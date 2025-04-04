1f27926d80f802331757c6076f4cbb2d56b9e7ac
Jose Luis Ibarra <josel.ibarra@congresosinaloa.mx>
2025-04-04 11:54:34 -0700

    Change title again v2

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -2,7 +2,7 @@
 export const TopBar = () => {
   return (
     <header className="bg-blue-300 text-blue-950 p-5 text-center">
-      <h1 className="font-black"><span className="text-white">Mini Proyecto de</span> Jose Luis en <span className="text-white font-light font-serif">NextJS</span></h1>
+      <h1 className="font-black"><span className="text-green-900 font-stretch-normal">Mini Proyecto de</span> Jose Luis en <span className="text-white font-light font-serif">NextJS</span></h1>
     </header>
   )
 }
